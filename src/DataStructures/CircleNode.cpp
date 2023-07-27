27bcd39f19c43651ad9a3bbbd5a78f79f31c8b70
BJMinhNhut <nhutben678@gmail.com>
2023-07-27 23:42:55 +0700

    Introduce CircleNode to support Graphs, Heap, AVL tree

--- src/DataStructures/CircleNode.cpp
+++ src/DataStructures/CircleNode.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "CircleNode.hpp"
+#include "Template/Utility.hpp"
+#include "Template/ResourceHolder.hpp"
+
+#include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+
+
+CircleNode::CircleNode(const FontHolder &fonts, const ColorHolder &colors)
+        : mValue(0),
+          mText(std::to_string(mValue), fonts.get(Fonts::Main), 20u),
+          mCircle(30.f) {
+    mText.setFillColor(colors.get(Colors::Text));
+    Utility::centerOrigin(mText);
+
+    mCircle.setFillColor(colors.get(Colors::UIPrimary));
+    mCircle.setOutlineColor(colors.get(Colors::UIBorder));
+    mCircle.setOutlineThickness(1.f);
+    Utility::centerOrigin(mCircle);
+}
+
+void CircleNode::updateCurrent(sf::Time dt) {}
+
+void CircleNode::drawCurrent(sf::RenderTarget &target,
+                             sf::RenderStates states) const {
+    target.draw(mCircle, states);
+    target.draw(mText, states);
+}
+
+int CircleNode::getValue() const {
+    return mValue;
+}
+
+void CircleNode::setValue(const int &value) {
+    setScale(0.f, 0.f);
+    mValue = value;
+    mText.setString(std::to_string(mValue));
+    Utility::centerOrigin(mText);
+}
+
+void CircleNode::setCircle(const sf::CircleShape &circle) {
+    mCircle = circle;
+}
--- src/DataStructures/CircleNode.hpp
+++ src/DataStructures/CircleNode.hpp
@@ -6,15 +6,40 @@
 #define DATAVISUALIZATION2_CIRCLENODE_HPP
 
 #include "Template/SceneNode.hpp"
+#include "Template/ColorHolder.hpp"
+#include "Template/ResourceIdentifiers.hpp"
+
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Text.hpp>
 
 /*
  * TODO: Circle Node implementation
- *  - [] Value display, with circle
+ *  - [x] Value display, with circle
  *  - [] Adjacent list, pointer to edges
  *  - [] When object moves, update the corresponding edge
  */
-class CircleNode: public SceneNode {
 
+class CircleNode : public SceneNode {
+public:
+    explicit CircleNode(const FontHolder &fonts,
+                        const ColorHolder &colors);
+
+    [[nodiscard]] int getValue() const;
+
+    void setValue(const int &value);
+
+    void setCircle(const sf::CircleShape &circle);
+
+private:
+    virtual void updateCurrent(sf::Time dt);
+
+    virtual void drawCurrent(sf::RenderTarget &target,
+                             sf::RenderStates states) const;
+
+private:
+    int mValue;
+    sf::Text mText;
+    sf::CircleShape mCircle;
 };
 
 
