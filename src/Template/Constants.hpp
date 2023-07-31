fea6a4059070588eaaeb10de0331ae1b28b3abf1
BJMinhNhut <nhutben678@gmail.com>
2023-07-31 19:02:04 +0700

    Fix wrong White color RGB, remove unnecessary constants

--- src/Template/Constants.hpp
+++ src/Template/Constants.hpp
@@ -12,22 +12,13 @@
 #include <vector>
 
 namespace Constants {
-// Drawing properties
-    float const NODE_SIZE = 40.f;
-    float const NODE_DISTANCE = 80.f;
-
-    int const NODE_MAXVALUE = 99;
-    int const NODE_MINVALUE = 0;
-    int const LIST_MAXSIZE = 10;
-    int const ARRAY_MAXSIZE = 10;
-
 // Math
     float const PI = 3.14159265;
 
 // Color
     sf::Color const mBlack(66, 66, 66);
     sf::Color const BlueDark(13, 71, 161);
-    sf::Color const WhiteDisplay(0, 0, 0);
+    sf::Color const WhiteDisplay(255, 255, 255);
     sf::Color const WhiteUI(245, 245, 245);
     sf::Color const GrayBorder(189, 189, 189);
     sf::Color const YellowLight(255, 245, 157);
@@ -41,8 +32,6 @@ namespace Constants {
     sf::Color const BlueGrayBorder(81, 105, 117);
 
 // Input
-    int const INPUT_MAX_LENGTH = 34;
-    int const INPUT_WRAP_LENGTH = 17;
     sf::Time const INPUT_CURSOR_LIFE(sf::milliseconds(400));
 
     // Resource links
