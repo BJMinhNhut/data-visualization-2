81720da0e431ab9845082358e06ed2233f6e93b2
BJMinhNhut <nhutben678@gmail.com>
2023-08-09 16:14:30 +0700

    PolyNode swapData

--- src/DataStructures/PolyNode.cpp
+++ src/DataStructures/PolyNode.cpp
@@ -3,65 +3,69 @@
 //
 
 #include "PolyNode.hpp"
-#include "Template/Utility.hpp"
 #include "Template/ResourceHolder.hpp"
+#include "Template/Utility.hpp"
 
 #include <SFML/Graphics/RenderStates.hpp>
 #include <SFML/Graphics/RenderTarget.hpp>
 
 #include <iostream>
 
-PolyNode::PolyNode(const FontHolder &fonts, const ColorHolder &colors)
-        : mText("", fonts.get(Fonts::Mono), 18u),
-          mPolygon(22.f), mColors(colors) {
-    mText.setFillColor(colors.get(Colors::Text));
-    Utility::centerOrigin(mText);
+PolyNode::PolyNode(const FontHolder& fonts, const ColorHolder& colors)
+    : mText("", fonts.get(Fonts::Mono), 18u), mPolygon(22.f), mColors(colors) {
+	mText.setFillColor(colors.get(Colors::Text));
+	Utility::centerOrigin(mText);
 
-    mPolygon.setFillColor(colors.get(Colors::UIPrimary));
-    mPolygon.setOutlineColor(colors.get(Colors::UIBorder));
-    mPolygon.setOutlineThickness(2.f);
-    Utility::centerOrigin(mPolygon);
+	mPolygon.setFillColor(colors.get(Colors::UIPrimary));
+	mPolygon.setOutlineColor(colors.get(Colors::UIBorder));
+	mPolygon.setOutlineThickness(2.f);
+	Utility::centerOrigin(mPolygon);
 }
 
 void PolyNode::updateCurrent(sf::Time dt) {
-    for (auto &edge: outEdges)
-        edge->update(dt);
-    for (auto &edge: inEdges)
-        edge->update(dt);
+	for (auto& edge : outEdges)
+		edge->update(dt);
+	for (auto& edge : inEdges)
+		edge->update(dt);
 }
 
-void PolyNode::drawCurrent(sf::RenderTarget &target,
-                           sf::RenderStates states) const {
-    for (auto &edge: outEdges)
-        target.draw(*edge, states);
-    target.draw(mPolygon, states);
-    target.draw(mText, states);
+void PolyNode::drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const {
+	for (auto& edge : outEdges)
+		target.draw(*edge, states);
+	target.draw(mPolygon, states);
+	target.draw(mText, states);
 }
 
 std::string PolyNode::getData() const {
-    return mText.getString();
+	return mText.getString();
 }
 
 int PolyNode::getIntData() const {
-    std::string data(mText.getString());
-    return stoi(data);
+	std::string data(mText.getString());
+	return stoi(data);
 }
 
-void PolyNode::setData(const std::string &data) {
-    mText.setString(data);
-    Utility::centerOrigin(mText);
+void PolyNode::setData(const std::string& data) {
+	mText.setString(data);
+	Utility::centerOrigin(mText);
 }
 
-void PolyNode::setData(const int &data) {
-    mText.setString(std::to_string(data));
-    Utility::centerOrigin(mText);
+void PolyNode::setData(const int& data) {
+	mText.setString(std::to_string(data));
+	Utility::centerOrigin(mText);
 }
 
-void PolyNode::setPoint(const int &points) {
-    mPolygon.setPointCount(points);
+void PolyNode::swapData(PolyNode* node) {
+	std::string temp = this->getData();
+	this->setData(node->getData());
+	node->setData(temp);
 }
 
-void PolyNode::addEdgeOut(PolyNode *to) {
+void PolyNode::setPoint(const int& points) {
+	mPolygon.setPointCount(points);
+}
+
+void PolyNode::addEdgeOut(PolyNode* to) {
 	auto edge = std::make_shared<Edge>(this, to, Edge::EdgeType::Undirected, mColors);
 	outEdges.push_back(edge);
 	to->addEdgeIn(edge);
@@ -71,23 +75,23 @@ void PolyNode::addEdgeIn(const Edge::Ptr& edge) {
 	inEdges.push_back(edge);
 }
 
-void PolyNode::removeEdgeOut(PolyNode *to) {
-    for (auto itr = outEdges.begin(); itr != outEdges.end(); ++itr) {
-        if ((*itr)->getTo() == to) {
+void PolyNode::removeEdgeOut(PolyNode* to) {
+	for (auto itr = outEdges.begin(); itr != outEdges.end(); ++itr) {
+		if ((*itr)->getTo() == to) {
 			to->removeEdgeIn(*itr);
 			outEdges.erase(itr);
-            return;
-        }
-    }
+			return;
+		}
+	}
 }
 
 void PolyNode::removeEdgeIn(const Edge::Ptr& edge) {
 	for (auto itr = inEdges.begin(); itr != inEdges.end(); ++itr) {
-        if (*itr == edge) {
-            inEdges.erase(itr);
-            return;
-        }
-    }
+		if (*itr == edge) {
+			inEdges.erase(itr);
+			return;
+		}
+	}
 }
 
 void PolyNode::removeAllEdges() {
--- src/DataStructures/PolyNode.hpp
+++ src/DataStructures/PolyNode.hpp
@@ -5,30 +5,30 @@
 #ifndef DATAVISUALIZATION2_POLYNODE_HPP
 #define DATAVISUALIZATION2_POLYNODE_HPP
 
-#include "Template/SceneNode.hpp"
+#include "Edge.hpp"
 #include "Template/ColorHolder.hpp"
 #include "Template/ResourceIdentifiers.hpp"
-#include "Edge.hpp"
+#include "Template/SceneNode.hpp"
 
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/Text.hpp>
 
 class PolyNode : public SceneNode {
-public:
-    explicit PolyNode(const FontHolder &fonts,
-                      const ColorHolder &colors);
+   public:
+	explicit PolyNode(const FontHolder& fonts, const ColorHolder& colors);
 
-    [[nodiscard]] std::string getData() const;
-    [[nodiscard]] int getIntData() const;
+	[[nodiscard]] std::string getData() const;
+	[[nodiscard]] int getIntData() const;
 
-    void setData(const std::string &data);
-    void setData(const int &data);
-    void setPoint(const int &points);
+	void setData(const std::string& data);
+	void setData(const int& data);
+	void swapData(PolyNode* node);
+	void setPoint(const int& points);
 
-    void addEdgeOut(PolyNode *to);
+	void addEdgeOut(PolyNode* to);
 	void addEdgeIn(const Edge::Ptr& edge);
-	void removeEdgeOut(PolyNode *to);
+	void removeEdgeOut(PolyNode* to);
 	void removeEdgeIn(const Edge::Ptr& edge);
 	void removeAllEdges();
 
@@ -36,17 +36,15 @@ public:
 	void setPosition(sf::Vector2f position) override;
 
    private:
-    void updateCurrent(sf::Time dt) override;
+	void updateCurrent(sf::Time dt) override;
 
-    void drawCurrent(sf::RenderTarget &target,
-                     sf::RenderStates states) const override;
+	void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const override;
 
-private:
-    sf::Text mText;
-    sf::CircleShape mPolygon;
+   private:
+	sf::Text mText;
+	sf::CircleShape mPolygon;
 	std::vector<Edge::Ptr> inEdges, outEdges;
-	const ColorHolder &mColors;
+	const ColorHolder& mColors;
 };
 
-
-#endif //DATAVISUALIZATION2_POLYNODE_HPP
+#endif  //DATAVISUALIZATION2_POLYNODE_HPP
