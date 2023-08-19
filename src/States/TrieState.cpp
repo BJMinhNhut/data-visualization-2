b16963aa8778a67de2eebab5b9383a9cc16e16d0
BJMinhNhut <nhutben678@gmail.com>
2023-08-19 17:03:25 +0700

    init Trie options

--- src/States/TrieState.cpp
+++ src/States/TrieState.cpp
@@ -10,6 +10,39 @@ TrieState::TrieState(StateStack& stack, State::Context context)
       mTrie(*context.fonts, *context.colors) {
 	mTrie.setTargetPosition(context.window->getSize().x / 2.f + 200.f, 180.f,
 	                        Trie::Transition::None);
+	initOptions();
+}
+
+void TrieState::initOptions() {
+	mActionsHub.addOption(Create, "Create", [&]() {
+		mActionsHub.setCurrentOption(Create);
+		mPlayer.reset();
+		mPlayer.callInfo("Init a new trie");
+	});
+	mActionsHub.addOption(Insert, "Insert", [&]() {
+		mActionsHub.setCurrentOption(Insert);
+		mPlayer.reset();
+		mPlayer.callInfo("Insert a new value to trie");
+		//		Inputs[Insert]->randomizeValue();
+	});
+	mActionsHub.addOption(Delete, "Delete", [&]() {
+		mActionsHub.setCurrentOption(Delete);
+		mPlayer.reset();
+		mPlayer.callInfo("Delete a value from trie");
+		//		if (mTree.getSize() > 0)
+		//			Inputs[Delete]->setValue(mTree.getRandomElement());
+		//		else
+		//			Inputs[Delete]->randomizeValue();
+	});
+	mActionsHub.addOption(Search, "Search", [&]() {
+		mActionsHub.setCurrentOption(Search);
+		mPlayer.reset();
+		mPlayer.callInfo("Search for a value in trie");
+		//		if (mTree.getSize() > 0)
+		//			Inputs[Search]->setValue(mTree.getRandomElement());
+		//		else
+		//			Inputs[Search]->randomizeValue();
+	});
 }
 
 void TrieState::draw() {
