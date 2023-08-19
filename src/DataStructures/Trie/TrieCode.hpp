c92961a846fb269f5074d3b10d199715396b8c95
BJMinhNhut <nhutben678@gmail.com>
2023-08-19 21:29:29 +0700

    Shorten trie code indent

--- src/DataStructures/Trie/TrieCode.hpp
+++ src/DataStructures/Trie/TrieCode.hpp
@@ -11,27 +11,27 @@ namespace TrieCode {
 const std::string Insert(
     "cur = root\n"
     "for(char c : str)\n"
-    "	if cur.child[c] == null\n"
-    "		cur.child[c] = new Node(c)\n"
-    "	cur = cur.child[c]\n"
-    "	cur.freq++\n"
+    "  if cur.child[c] == null\n"
+    "    cur.child[c] = new Node(c)\n"
+    "  cur = cur.child[c]\n"
+    "  cur.freq++\n"
     "cur.endStr = true");
 
 const std::string Delete(
     "cur = search(str)\n"
     "cur.endStr = false\n"
     "for(char ch : rev(str))\n"
-    "	cur.freq--\n"
-    "	cur = cur.parent\n"
-    "	if cur.child[ch].freq == 0\n"
-    "		delete cur.child[ch]");
+    "  cur.freq--\n"
+    "  cur = cur.parent\n"
+    "  if cur.child[ch].freq == 0\n"
+    "    delete cur.child[ch]");
 
 const std::string Search(
     "cur = root\n"
     "for(char c : str)\n"
-    "	if cur.child[c] == null\n"
-    "		return NOT_FOUND\n"
-    "	cur = cur.child[c]\n"
+    "  if cur.child[c] == null\n"
+    "    return NOT_FOUND\n"
+    "  cur = cur.child[c]\n"
     "if cur.endStr return FOUND\n"
     "else return NOT_FOUND");
 }  // namespace TrieCode
