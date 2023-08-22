ed90dcf4011d153f247a19aec2ae77e830dbb155
BJMinhNhut <nhutben678@gmail.com>
2023-08-22 21:07:10 +0700

    Add dim border color

--- src/Template/Constants.hpp
+++ src/Template/Constants.hpp
@@ -24,6 +24,7 @@ sf::Color const GrayBorder(189, 189, 189);
 sf::Color const YellowLight(255, 245, 157);
 sf::Color const OrangeLight(255, 213, 79);
 sf::Color const RedDark(183, 28, 28);
+sf::Color const DimLightBorder(189, 189, 189, 50);
 
 sf::Color const BlackUI(69, 90, 100);
 sf::Color const BlackDisplay(55, 71, 79);
@@ -32,6 +33,7 @@ sf::Color const BlueLight(100, 181, 246);
 sf::Color const TealDark(0, 96, 100);
 sf::Color const TealLight(0, 137, 123);
 sf::Color const BlueGrayBorder(81, 105, 117);
+sf::Color const DimDarkBorder(81, 105, 117, 50);
 
 // Resource links
 std::vector<std::string> const imageNames = {
